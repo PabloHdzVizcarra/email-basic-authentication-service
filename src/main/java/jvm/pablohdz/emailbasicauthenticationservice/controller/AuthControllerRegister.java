4e8176eeb53b9e0fc1f4f7a18f9b8373a8f04f09
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-10-03 11:46:05 -0500

    feat read data from request

--- src/main/java/jvm/pablohdz/emailbasicauthenticationservice/controller/AuthControllerRegister.java
+++ src/main/java/jvm/pablohdz/emailbasicauthenticationservice/controller/AuthControllerRegister.java
@@ -1,14 +1,27 @@
 package jvm.pablohdz.emailbasicauthenticationservice.controller;
 
+import com.google.common.base.Charsets;
+import com.google.common.io.CharStreams;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class AuthControllerRegister implements HttpHandler {
     @Override
-    public void handle(HttpExchange exchange) throws IOException {
+    public void handle(HttpExchange exchange) {
         String requestMethod = exchange.getRequestMethod();
-        System.out.println(requestMethod);
+
+        InputStream requestBody = exchange.getRequestBody();
+        try {
+            String body =
+                    CharStreams.toString(new InputStreamReader(requestBody, Charsets.UTF_8));
+
+            System.out.println(body);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
