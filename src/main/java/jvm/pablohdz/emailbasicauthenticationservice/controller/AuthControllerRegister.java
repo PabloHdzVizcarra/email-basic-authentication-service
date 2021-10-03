6b2f93b74bbb062485d80ef24b63364c19f011dc
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-10-03 11:56:59 -0500

    feat parse json body to object java

--- src/main/java/jvm/pablohdz/emailbasicauthenticationservice/controller/AuthControllerRegister.java
+++ src/main/java/jvm/pablohdz/emailbasicauthenticationservice/controller/AuthControllerRegister.java
@@ -1,5 +1,6 @@
 package jvm.pablohdz.emailbasicauthenticationservice.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.base.Charsets;
 import com.google.common.io.CharStreams;
 import com.sun.net.httpserver.HttpExchange;
@@ -10,16 +11,23 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 
 public class AuthControllerRegister implements HttpHandler {
+
     @Override
     public void handle(HttpExchange exchange) {
         String requestMethod = exchange.getRequestMethod();
 
         InputStream requestBody = exchange.getRequestBody();
         try {
+
             String body =
                     CharStreams.toString(new InputStreamReader(requestBody, Charsets.UTF_8));
 
-            System.out.println(body);
+            ObjectMapper objectMapper = new ObjectMapper();
+            UserRequest user = objectMapper.readValue(body, UserRequest.class);
+            System.out.println(user);
+
+
+
         } catch (IOException e) {
             e.printStackTrace();
         }
---
+++ src/main/java/jvm/pablohdz/emailbasicauthenticationservice/controller/UserRequest.java
@@ -0,0 +1,61 @@
+package jvm.pablohdz.emailbasicauthenticationservice.controller;
+
+
+public class UserRequest {
+    private String name;
+    private String lastname;
+    private String username;
+    private String email;
+    private String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "UserRequest{" +
+                "name='" + name + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
