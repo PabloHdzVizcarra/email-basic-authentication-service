f4b58e7f48c0e776633d4683c6c1c28114e7cc85
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2021-10-06 21:45:29 -0500

    fix type parameter

--- src/main/java/jvm/pablohdz/emailbasicauthenticationservice/dao/UserDao.java
+++ src/main/java/jvm/pablohdz/emailbasicauthenticationservice/dao/UserDao.java
@@ -2,11 +2,12 @@ package jvm.pablohdz.emailbasicauthenticationservice.dao;
 
 import jvm.pablohdz.emailbasicauthenticationservice.controller.UserRequest;
 import jvm.pablohdz.emailbasicauthenticationservice.persistence.SqlDatabase;
+import jvm.pablohdz.emailbasicauthenticationservice.service.Database;
 
 public class UserDao {
-    private final SqlDatabase database;
+    private final Database database;
 
-    public UserDao(SqlDatabase database) {
+    public UserDao(Database database) {
         this.database = database;
     }
 
